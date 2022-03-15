5dae31afc73009c09248b0730db0e5eb1091ec06
João Martins <joaolucassilvamartins@gmail.com>
2022-03-15 10:35:51 +0000

    Add: Initial setup script and ssh keys and config

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+from netmiko import ConnectHandler, file_transfer
+from netmiko import SCPConn
+
+device_A = {
+    'device_type': 'linux',
+    'ip': '192.168.109.155', # Our PC is 5
+    'username': 'theuser',
+    'use_keys': True,
+    'key_file': './ssh/keys/privA.rsa'
+}
+
+def sendFiles(ssh_con, files):
+    scp_conn = SCPConn(ssh_con)
+    for (source, dest) in files:
+        scp_conn.scp_transfer_file(source, dest)
+    scp_conn.close()
+
+def initConfig():
+    ssh_A = ConnectHandler(**device_A)
+    ssh_A.enable()
+
+    # Send B and C keys to A
+    sendBCKey = ("ssh/keys/privBC.rsa", ".ssh/")
+    sendAConfig = ("ssh/configA.ssh", ".ssh/config")
+    sendFiles(ssh_A, [sendBCKey, sendAConfig])
+
+    ssh_A.disconnect()
+
+initConfig()
