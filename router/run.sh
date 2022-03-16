4d1509d46dbca3707aed69202e666ec7c399961d
João Martins <joaolucassilvamartins@gmail.com>
2022-03-16 15:50:27 +0000

    Add: Working net

--- router/run.sh
+++ router/run.sh
@@ -1,2 +1,3 @@
 docker build -t router-img .
-docker run -d --name router router-img
+docker run -d --net client_net --ip 10.0.1.254 --cap-add=NET_ADMIN --name router router-img
+docker network connect server_net router --ip 10.0.2.254
--- server/run.sh
+++ server/run.sh
@@ -1,2 +1,5 @@
 docker build -t server-img .
-docker run -d --name server server-img
+# docker network create -d macvlan --subnet=10.0.2.0/24 --gateway=10.0.2.1 -o parent=ens20 server_net
+docker run -d --net server_net --ip 10.0.2.100 --cap-add=NET_ADMIN --name server server-img
+docker exec server /bin/bash -c 'ip r del default via 10.0.2.1'
+docker exec server /bin/bash -c 'ip r a 10.0.1.0/24 via 10.0.2.254'
