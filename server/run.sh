43eb32c78e8c3fabefbe7b26f5f137cd6549eae9
2dukes <ruipinto02@hotmail.com>
2022-04-05 10:38:29 +0100

    Update: Configs | Add: check_http.

--- server/run.sh
+++ server/run.sh
@@ -1,3 +1,3 @@
-docker-compose -f server/docker-compose.yml up -d
+docker-compose -f server/docker-compose.yml up -d --build
 docker exec server /bin/bash -c 'ip r del default via 10.0.2.1'
 docker exec server /bin/bash -c 'ip r a 10.0.1.0/24 via 10.0.2.254'
