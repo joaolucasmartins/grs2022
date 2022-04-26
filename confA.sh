2a380949e3dda6c2a98f008318a452e488ed50ff
2dukes <ruipinto02@hotmail.com>
2022-04-26 18:40:43 +0100

    Fix: Small stuff.

--- confA.sh
+++ confA.sh
@@ -6,5 +6,3 @@ sudo sysctl -w net.ipv4.ip_forward=1
 # sudo iptables -t nat -A POSTROUTING -s 192.168.88.101 -o eth0 -j MASQUERADE
 # sudo iptables -t nat -A POSTROUTING -s 192.168.88.102 -o eth0 -j MASQUERADE # Give VM-C access to the internet.
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-sudo ip r a 10.0.2.0/24 via 192.168.88.101
-sudo ip r a 10.0.1.0/24 via 192.168.88.101
