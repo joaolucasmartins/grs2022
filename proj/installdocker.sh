8577294664e822e5f776c14dbf13123745c19d58
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-05-15 15:32:43 +0100

    Add: docker install script + network config scripts.

---
+++ proj/installdocker.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# remove old docker versions
+sudo apt-get -y remove docker docker-engine docker.io containerd runc
+
+# deps
+sudo apt-get -y update
+sudo apt-get -y install \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release
+
+# docker official gpg key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# docker stable repo
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# install docker engine
+sudo apt-get -y update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+# compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
