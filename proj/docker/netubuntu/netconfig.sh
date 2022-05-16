f05f1f34acc5ff26262b55ec688b83215c4dc190
2dukes <ruipinto02@hotmail.com>
2022-05-16 15:29:44 +0100

    Add: Connection between internal and DMZ Porto's networks.

---
+++ proj/docker/netubuntu/netconfig.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ip_default_gateway=$1
+ip_new_default_gateway=$2
+
+ip r d default via "$ip_default_gateway"
+ip r a default via "$ip_new_default_gateway"
+
+/bin/sh -c "tail -f /dev/null"
