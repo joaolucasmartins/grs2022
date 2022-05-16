0ca0a599b7db6cf6fdae18a4478025eee20f22d0
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-05-16 14:42:08 +0100

    Bizings

--- proj/helper_scripts/configmachine_network.sh
+++ proj/helper_scripts/configmachine_network.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+sudo ip a add 192.168.88.100/24 dev ens19
+sudo ip r add 192.168.88.0/24 vid 192.168.88.100
+
 sudo sysctl -w net.ipv4.ip_forward=1
 sudo iptables -t nat -A POSTROUTING -s 192.168.88.101 -o eth0 -j MASQUERADE
 sudo iptables -t nat -A POSTROUTING -s 192.168.88.102 -o eth0 -j MASQUERADE
