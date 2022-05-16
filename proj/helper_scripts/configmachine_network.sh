2f7efd593c2e1523f582a353dd4ae9a5f1da9eb3
anaines14 <up201806593@fe.up.pt>
2022-05-16 15:00:14 +0100

    Fix: scripts

--- proj/helper_scripts/configmachine_network.sh
+++ proj/helper_scripts/configmachine_network.sh
@@ -2,6 +2,7 @@
 
 sudo ip a add 192.168.88.100/24 dev ens19
 sudo ip r add 192.168.88.0/24 via 192.168.88.100
+sudo ip link set ens19 up
 
 sudo sysctl -w net.ipv4.ip_forward=1
 sudo iptables -t nat -A POSTROUTING -s 192.168.88.101 -o eth0 -j MASQUERADE
