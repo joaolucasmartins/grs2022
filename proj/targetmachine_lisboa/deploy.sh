17107d235e95f6dfbe5e84fb61ac64e87a97f5ee
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-05-17 20:25:00 +0100

    Fix: test timeouts

--- proj/targetmachine_lisboa/deploy.sh
+++ proj/targetmachine_lisboa/deploy.sh
@@ -13,11 +13,11 @@ sudo systemctl start docker-compose@lisboa
 echo "Run tests:"
 
 printf "\t1. curl the webapp proxy"
-sudo docker exec "r_b" curl 10.0.1.5
+sudo docker exec "r_b" curl 10.0.1.5 2>/dev/null
 
 printf "\t2. webdevs getting leases from DHCP server"
-#sudo docker exec "webdev1" /bin/sh -c "dhcpcd -t 1 2>&1 | grep '.* leased .* for .* seconds'"
-#sudo docker exec "webdev2" /bin/sh -c "dhcpcd -t 1 2>&1 | grep '.* leased .* for .* seconds'"
-#
-#printf "\t3. access to pgadmin"
-#sudo docker exec "webdev1" curl 10.0.2.3
+sudo docker exec "webdev1" /bin/sh -c "dhcpcd -t 15 2>&1 | grep '.* leased .* for .* seconds'"
+sudo docker exec "webdev2" /bin/sh -c "dhcpcd -t 15 2>&1 | grep '.* leased .* for .* seconds'"
+
+printf "\t3. access to pgadmin"
+sudo docker exec "webdev1" curl 10.0.2.3 2>/dev/null
