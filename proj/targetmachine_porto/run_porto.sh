fada1f2d0eccc807535be98bdf85cbe298833b44
João Martins <joaolucassilvamartins@gmail.com>
2022-05-17 11:20:10 +0100

    WIP: Add switch config

--- proj/targetmachine_porto/run_porto.sh
+++ proj/targetmachine_porto/run_porto.sh
@@ -2,3 +2,7 @@
 
 ssh vmc 'bash -s' < ../helper_scripts/targetmachine_network.sh
 ssh vmc 'bash -s' < ../helper_scripts/installdocker.sh
+ssh vmc 'bash -s' < ./config_c.sh
+
+scp -r ../switch/switch_bridges.ssh switch-ssh:/scripts.to.run
+ssh switch-ssh '/import /scripts.to.run'
