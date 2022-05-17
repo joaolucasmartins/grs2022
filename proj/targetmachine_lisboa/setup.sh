e95e0fd885017ef9ce2435b59be6f5238aa881b4
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-05-17 14:49:26 +0100

    WIP

--- proj/targetmachine_lisboa/setup.sh
+++ proj/targetmachine_lisboa/setup.sh
@@ -8,19 +8,11 @@ sudo docker network rm "webapp_net" "internal_net" "dmz_net"
 # Create networks
 echo "Creating networks"
 sudo ip link set ens19 up
-sudo docker network create -d macvlan --subnet=10.0.3.0/24 --gateway=10.0.3.254 -o parent=ens19.3 webapp_net
-sudo docker network create -d macvlan --subnet=10.0.2.0/24 --gateway=10.0.2.254 -o parent=ens19.2 internal_net
-sudo docker network create -d macvlan --subnet=10.0.1.0/24 --gateway=10.0.1.254 -o parent=ens19.1 dmz_net
 
 # Deploy router
 echo "Deploying router"
 sudo docker run -d --name "r1" router 
-
-# Connect router to networks
-echo "Connecting router to internal network"
-sudo docker network connect "internal_net" r1 --ip "10.0.2.1" 
-echo "Connecting router to DMZ network"
-sudo docker network connect "dmz_net" r1 --ip "10.0.1.2" 
+sudo systemctl start docker-compose@lisboa
 
 # Server and workers
 echo "Deploying workers"
