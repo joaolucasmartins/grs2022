8fb4cac0fb4be9e61963a594304a77e967fc40e8
João Martins <joaolucassilvamartins@gmail.com>
2022-05-17 18:51:04 +0100

    Add & Fix: Docker compose service to porto

--- proj/targetmachine_porto/deploy.sh
+++ proj/targetmachine_porto/deploy.sh
@@ -2,7 +2,7 @@
 
 # TODO Put this config file
 
-# python3 network_conf.py netarchitecture.json docker-compose.yml | ssh vmc 'tee docker-compose.yml' > /dev/null
+# python3 network_conf.py netarchitecture.json docker-compose_tpl.yml | ssh vmc 'tee docker-compose.yml' > /dev/null
 
 # Config switch
 scp -r ../switch/switch_bridges.ssh switch-ssh:/scripts.to.run
