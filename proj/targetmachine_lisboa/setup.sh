b61feaa076343a6a9e4dfcc870f7688dde58a311
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-05-17 23:50:51 +0100

    Better test messages.

--- proj/targetmachine_lisboa/setup.sh
+++ proj/targetmachine_lisboa/setup.sh
@@ -14,5 +14,8 @@ scp "../docker/docker-compose@.service" vmb:~
 ssh vmb 'bash' <"../helper_scripts/setup_compose_service.sh"
 
 echo "Copy docker containers to the machine"
-scp -r "../docker/dhcp" "../docker/router" "../docker/edge_router" "../docker/webapp" "../docker/webapp_worker" "../docker/webdev" "docker-compose.yml" vmb:~/docker
+scp -r "../docker/dhcp" "../docker/router" "../docker/edge_router" "../docker/webapp" "../docker/webapp_worker" "../docker/webdev" vmb:~/docker
+echo "Configure machine from template"
+python3 "../helper_scripts/network_conf.py" "netarchitecture.json" "docker-compose_tpl.yml" | ssh vmb 'tee ~/docker/docker-compose.yml' >/dev/null
+echo "Setup containers for usage"
 ssh vmb 'bash' <"./setup_containers.sh"
