497fca0f482fa38dc7e26ca5fd6345bcb734d347
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-05-17 19:54:48 +0100

    Cool webdevs and dhcp

--- proj/targetmachine_lisboa/setup.sh
+++ proj/targetmachine_lisboa/setup.sh
@@ -4,15 +4,15 @@ echo "Disable automatic updates (so we can reliably install docker)"
 #ssh vmb 'bash' <"../helper_scripts/disable-automatic-updates.sh"
 
 echo "Config network"
-ssh vmb 'bash' <"../helper_scripts/targetmachine_network.sh"
+#ssh vmb 'bash' <"../helper_scripts/targetmachine_network.sh"
 
 echo "Install docker"
-ssh vmb 'bash' <"../helper_scripts/installdocker.sh"
+#ssh vmb 'bash' <"../helper_scripts/installdocker.sh"
 
 echo "Setup docker compose service"
 scp "../docker/docker-compose@.service" vmb:~
 ssh vmb 'bash' <"../helper_scripts/setup_compose_service.sh"
 
 echo "Copy docker containers to the machine"
-scp -r  "../docker/dhcp" "../docker/router" "../docker/webapp" "../docker/webapp_worker" "docker-compose.yml" vmb:~/docker
+scp -r "../docker/dhcp" "../docker/router" "../docker/webapp" "../docker/webapp_worker" "../docker/webdev" "docker-compose.yml" vmb:~/docker
 ssh vmb 'bash' <"./setup_containers.sh"
