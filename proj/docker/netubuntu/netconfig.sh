33bb9969fe0d7cec412fc05eeb10c4a525475084
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-05-17 14:50:06 +0100

    Pensive

--- proj/docker/netubuntu/netconfig.sh
+++ proj/docker/netubuntu/netconfig.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ip_default_gateway=$1
-ip_new_default_gateway=$2
+ip_default_gateway="$1"
+ip_new_default_gateway="$2"
 
 ip r d default via "$ip_default_gateway"
 ip r a default via "$ip_new_default_gateway"
