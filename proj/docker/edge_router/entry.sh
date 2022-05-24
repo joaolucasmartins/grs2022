1fc450a022323aa385263eaf5fb2fed0f19c6495
2dukes <ruipinto02@hotmail.com>
2022-05-24 22:18:03 +0100

    Add: Firewall as MiTM | Fix: DNS resolving for external queries.

--- proj/docker/edge_router/entry.sh
+++ proj/docker/edge_router/entry.sh
@@ -3,19 +3,25 @@
 dflt_gateway="$1"
 dmz_ip="$2"
 internal_network="$3"
-internal_gateway="$4"
+gateway="$4"
 
 ip r del default
 ip r add default via "$dflt_gateway"
 
-ip r add "$internal_network" via "$internal_gateway"
+ip r del "$dmz_ip"
+ip r add "$gateway" dev eth0
+ip r add "$dmz_ip" via "$gateway"
+ip r add "$internal_network" via "$gateway"
+
+# ip r add "$internal_network" via "$gateway"
+# ip r add "$dmz_ip" via "$gateway"
 
 iptables -t nat -F
 iptables -t filter -F
 iptables -t nat -A POSTROUTING -j MASQUERADE
-iptables -P FORWARD DROP
-iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A FORWARD -m state --state NEW -i eth0 -j ACCEPT
+# iptables -P FORWARD DROP
+# iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
+# iptables -A FORWARD -m state --state NEW -i eth0 -j ACCEPT
 # iptables -A FORWARD -m state --state NEW -i eth1 -d "$dmz_ip" -j ACCEPT
 
 tail -f "/dev/null"
