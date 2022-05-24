b4653e3ed5d6c70779d0151c7e1596964b0b2ea0
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-05-24 19:11:39 +0100

    Vroom

--- proj/docker/router/entry.sh
+++ proj/docker/router/entry.sh
@@ -7,10 +7,16 @@ ip r add default via "$dflt_gateway"
 
 # drop DHCP requests that attempt to leave the local network
 #iptables -A INPUT -p udp --dport 67:68 -j DROP
-#iptables -A INPUT -p tcp --dport 67:68 -j DROP
 #iptables -A FORWARD -p udp --dport 67:68 -j DROP
-#iptables -A FORWARD -p tcp --dport 67:68 -j DROP
 #iptables -A OUTPUT -p udp --dport 67:68 -j DROP
+#iptables -A INPUT -p udp --sport 67:68 -j DROP
+#iptables -A FORWARD -p udp --sport 67:68 -j DROP
+#iptables -A OUTPUT -p udp --sport 67:68 -j DROP
+#iptables -A INPUT -p tcp --dport 67:68 -j DROP
+#iptables -A FORWARD -p tcp --dport 67:68 -j DROP
 #iptables -A OUTPUT -p tcp --dport 67:68 -j DROP
+#iptables -A INPUT -p tcp --sport 67:68 -j DROP
+#iptables -A FORWARD -p tcp --sport 67:68 -j DROP
+#iptables -A OUTPUT -p tcp --sport 67:68 -j DROP
 
 tail -f "/dev/null"
