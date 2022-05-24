95fc321675aa5c88bfc0eb95d162e8dd413b1286
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-05-24 18:33:43 +0100

    Fix: problem in nat

--- proj/docker/router/entry.sh
+++ proj/docker/router/entry.sh
@@ -5,8 +5,12 @@ dflt_gateway="$1"
 ip r del default
 ip r add default via "$dflt_gateway"
 
-# drop DHCp requests that attempt to leave the local network
-iptables -A FORWARD -p udp --dport 67 -j DROP
-iptables -A FORWARD -p udp --sport 68 -j DROP
+# drop DHCP requests that attempt to leave the local network
+#iptables -A INPUT -p udp --dport 67:68 -j DROP
+#iptables -A INPUT -p tcp --dport 67:68 -j DROP
+#iptables -A FORWARD -p udp --dport 67:68 -j DROP
+#iptables -A FORWARD -p tcp --dport 67:68 -j DROP
+#iptables -A OUTPUT -p udp --dport 67:68 -j DROP
+#iptables -A OUTPUT -p tcp --dport 67:68 -j DROP
 
 tail -f "/dev/null"
