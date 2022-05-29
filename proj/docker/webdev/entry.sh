6bd8be039d95081f0f82c449c8fe85f64050f0c9
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-05-29 20:10:59 +0100

    Aprende cucs

--- proj/docker/webdev/entry.sh
+++ proj/docker/webdev/entry.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-# get IP from DHCP server
+# enable DNS updates
+resolvconf --enable-updates
+
+# get IP, routes, and name servers from DHCP server
 dhcpcd
+
+# update DNS stuff
+resolvconf -u
+# overwrite resolv.conf because we can't link it
+tee /etc/resolv.conf </run/resolvconf/resolv.conf >/dev/null
 
 tail -f /dev/null
