cdd0e3e2a4a2610f369ddbf2b57614bb01f17f30
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-05-30 16:27:23 +0100

    Fix: python script raw strring thing

--- proj/common_scripts/network_conf.py
+++ proj/common_scripts/network_conf.py
@@ -11,31 +11,40 @@ net_arch = json.load(f)
 
 sed_expressions = []
 for container in net_arch:
-  container_name = container["name"]
+    container_name = container["name"]
 
-  for node in container:
-    if node == "networks":
-      for network in container["networks"]:
-        ip = network["ip"]
-        name = network["name"]
-        sed_expressions.extend(["-e", f"s|{container_name}_{name}|{ip}|ig"])
-    elif node == "entrypoints":
-      entrypoints_len = len(container["entrypoints"]) if "entrypoints" in container else 0
-      if entrypoints_len:
-        entrypoint_cmd = "/bin/sh -c '"
-        entrypoint_args = []
-        for i in range(entrypoints_len):
-          entrypoint = container["entrypoints"][i]
-          entrypoint_args.append("{path} {arguments}"
-                         .format(path=entrypoint["path"], arguments=" ".join(entrypoint["commands"])) 
-                         if "commands" in entrypoint else "{path}".format(path=entrypoint["path"]))
-                         
-        entrypoint_cmd += " \&\& ".join(entrypoint_args) + "'"
-        sed_expressions.extend(["-e", f"s|{container_name}_entrypoint|{entrypoint_cmd}|ig"])
-    elif node != "name": # Fill other params of container
-        val = container[node]
-        sed_expressions.extend(["-e", f"s|{container_name}_{node}|\"{val}\"|ig"])
-  
+    for node in container:
+        if node == "networks":
+            for network in container["networks"]:
+                ip = network["ip"]
+                name = network["name"]
+                sed_expressions.extend(["-e", f"s|{container_name}_{name}|{ip}|ig"])
+        elif node == "entrypoints":
+            entrypoints_len = (
+                len(container["entrypoints"]) if "entrypoints" in container else 0
+            )
+            if entrypoints_len:
+                entrypoint_cmd = "/bin/sh -c '"
+                entrypoint_args = []
+                for i in range(entrypoints_len):
+                    entrypoint = container["entrypoints"][i]
+                    entrypoint_args.append(
+                        "{path} {arguments}".format(
+                            path=entrypoint["path"],
+                            arguments=" ".join(entrypoint["commands"]),
+                        )
+                        if "commands" in entrypoint
+                        else "{path}".format(path=entrypoint["path"])
+                    )
+
+                entrypoint_cmd += r" \&\& ".join(entrypoint_args) + "'"
+                sed_expressions.extend(
+                    ["-e", f"s|{container_name}_entrypoint|{entrypoint_cmd}|ig"]
+                )
+        elif node != "name":  # Fill other params of container
+            val = container[node]
+            sed_expressions.extend(["-e", f's|{container_name}_{node}|"{val}"|ig'])
+
 
 f.close()
 subprocess.call(["sed", *sed_expressions, compose_file])
