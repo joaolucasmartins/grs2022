1f76e4eb3915d91b8534cf0fce4d91996452424f
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-05-31 15:43:55 +0100

    Dei branch da master

--- proj/setup.sh
+++ proj/setup.sh
@@ -12,8 +12,8 @@ containers="$3"
 echo "Config network"
 ssh "$machine_ssh" 'bash' <"./common_scripts/targetmachine_network.sh"
 
-echo "Install docker"
-ssh "$machine_ssh" 'bash' <"./common_scripts/installdocker.sh"
+#echo "Install docker"
+#ssh "$machine_ssh" 'bash' <"./common_scripts/installdocker.sh"
 
 echo "Setup docker compose service"
 scp "./docker/docker-compose@.service" "$machine_ssh":~
