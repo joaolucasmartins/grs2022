69e0cd9f58a697a0ec900ddca1867d0e05663413
JoaoCostaIFG <joaocosta.work@posteo.net>
2022-06-09 11:09:13 +0100

    Update: move from scp to rsync.

--- proj/setup.sh
+++ proj/setup.sh
@@ -8,30 +8,41 @@ containers="$3"
 
 #echo "Disable automatic updates (so we can reliably install docker)"
 #ssh vmb 'bash' <"./common_scripts/disable-automatic-updates.sh"
+#echo
 
 echo "Config network"
 ssh "$machine_ssh" 'bash' <"./common_scripts/targetmachine_network.sh"
+echo
+
+echo "Install rsync"
+ssh "$machine_ssh" 'sudo apt-get -y install rsync'
+echo
 
 echo "Install docker"
 ssh "$machine_ssh" 'bash' <"./common_scripts/installdocker.sh"
+echo
 
 echo "Setup docker compose service"
-scp "./docker/docker-compose@.service" "$machine_ssh":~
+rsync "./docker/docker-compose@.service" "$machine_ssh":~
 ssh "$machine_ssh" 'bash' <"./common_scripts/setup_compose_service.sh"
+echo
 
 echo "Copy docker containers to the machine"
 PREV_IFS="$IFS"
 IFS=":"
 for container in $containers; do
-	scp -r "./docker/${container}" "$machine_ssh":~/docker
+  echo "Copying ${container}..."
+	rsync -a -r --delete "./docker/${container}" "$machine_ssh":~/docker
 done
 IFS="$PREV_IFS"
+echo
 
 echo "Configure machine from template"
 python3 "./common_scripts/network_conf.py" "./${name}_machine/netarchitecture.json" "./${name}_machine/docker-compose_tpl.yml" |
 	ssh "$machine_ssh" 'tee ~/docker/docker-compose.yml' >/dev/null
+echo
 
-echo "Setup containers for usage"
+echo "Setup services' containers for deployment"
 ssh "$machine_ssh" sudo rm -rf "/etc/docker/compose/${name}"
 ssh "$machine_ssh" sudo mkdir -p "/etc/docker/compose/${name}"
 ssh "$machine_ssh" sudo mv "~/docker/"* "/etc/docker/compose/${name}"
